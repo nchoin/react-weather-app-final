05af2a6058e6a4d3580d78a22ea0b1edb00e4ee5
nchoin <139597297+nchoin@users.noreply.github.com>
2024-07-12 19:25:43 -0400

    added a footer with links to GitHub repo, my web portfolio, and website on Netlify

--- src/App.js
+++ src/App.js
@@ -2,6 +2,28 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://nck-portfolio-creator-developer.netlify.app/"
+          target="_blank"
+        >
+          Nicole Choiniere-Kroeker
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/nchoin/react-weather-app-final"
+          target="_blank"
+        >
+          GitHub
+        </a>
+        {" "} and hosted on <a href="https://weather-app-react-shecodes-final-nck.netlify.app/" target="_blank">Netlify</a>
+      </footer>
+    </div>
+  );
 }
 
