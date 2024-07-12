7de89f4d5c7f7cf38197032f96c1257db5508bbb
nchoin <139597297+nchoin@users.noreply.github.com>
2024-07-12 19:50:05 -0400

    added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,34 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
+      <div className="container">
+        
+        <h1> Weather App</h1>
 
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://nck-portfolio-creator-developer.netlify.app/"
-          target="_blank"
-        >
-          Nicole Choiniere-Kroeker
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/nchoin/react-weather-app-final"
-          target="_blank"
-        >
-          GitHub
-        </a>
-        {" "} and hosted on <a href="https://weather-app-react-shecodes-final-nck.netlify.app/" target="_blank">Netlify</a>
-      </footer>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://nck-portfolio-creator-developer.netlify.app/"
+            target="_blank"
+          >
+            Nicole Choiniere-Kroeker
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/nchoin/react-weather-app-final"
+            target="_blank"
+          >
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://weather-app-react-shecodes-final-nck.netlify.app/"
+            target="_blank"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
