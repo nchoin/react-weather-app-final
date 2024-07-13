ccca5e04b4851de805d68e645f66aaa682c68753
nchoin <139597297+nchoin@users.noreply.github.com>
2024-07-13 13:15:56 -0400

    added a state to create an object after making  an API a

--- src/Weather.js
+++ src/Weather.js
@@ -1,50 +1,75 @@
-import React from "react";
+import React , {useState} from "react";
 import axios from "axios";
 import "./Weather.css";
 export default function Weather(){
-    const apiKey = "6bccfefa354f0f4do4245dc0a56fata0";
-    let city = "New York"
+    // const [ready, setReady] = useState(false);
+    const [weatherData, setWeatherData]=useState({ready:false});
+// handleResponse function will use the date from the api call and display it however I decide.
+    function handleResponse(response){
+        console.log(response.data)
+        setWeatherData({
+          ready:true,
+          city: response.data.city,
+          temperature: Math.round(response.data.temperature.current),
+          date: "Friday 07:58",
+          humidity: response.data.temperature.humidity,
+          wind: Math.round(response.data.wind.speed),
+          conditions: response.data.condition.description,
+          icon: response.data.condition.icon_url,
+          iconDescription:response.data.condition.icon,
+        });
+        
+    }
+
+    if (weatherData.ready) {
+        return (
+          <div className="Weather">
+            <form>
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city"
+                    className="form-control"
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary"
+                  />
+                </div>
+              </div>
+            </form>
+            <h1>{weatherData.city}</h1>
+            <ul>
+              <li>{weatherData.date}</li>
+              <li class="text-capitalize">{weatherData.conditions}</li>
+            </ul>
+            <div className="row">
+              <div className="col-6">
+                <img src={weatherData.icon} alt={weatherData.iconDescription} />
+                {weatherData.temperature}°F
+              </div>
+              <div className="col-6">
+                <ul>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {weatherData.wind}mph</li>
+                </ul>
+              </div>
+            </div>
+            This is from the Weather component
+          </div>
+        );
+    } else {
+            const apiKey = "6bccfefa354f0f4do4245dc0a56fata0";
+    let city = "Cranston"
     const apiUrl =
       `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city"
-                className="form-control"
-              />
-            </div>
-            <div className="col-3">
-                 <input type="submit" value="Search" className="btn btn-primary" />
-            </div>
-           
-          </div>
-        </form>
-        <h1>New York</h1>
-        <ul>
-          <li>Friday 07:58</li>
-          <li>Mostly Cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="partly cloudy"
-            />
-            82°F
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation 15%</li>
-              <li>Humidity 76%</li>
-              <li>Wind 9mph</li>
-            </ul>
-          </div>
-        </div>
-        This is from the Weather component
-      </div>
-    );
+    axios.get(apiUrl).then(handleResponse)
+    return "loading...Please wait..."
+    }
+
+    
 }
